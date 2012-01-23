2842e0e5e128d94f5d3e25246e16b5443e9169cc
Moe Burney <moeburney@gmail.com>
2012-01-22 20:57:48 -0500

    usa map is now generated by gui

--- scripts/classes/map.py
+++ scripts/classes/map.py
@@ -8,33 +8,32 @@ class Map(object):
         self.image = None
         self.x = 800
         self.y = 600
-        self.width_ratio = 1
-        self.height_ratio = 1
+        self.width_ratio = 0.75
+        self.height_ratio = 0.75
 
-    def _get_region_coords(self, region):
-        """Returns the coords for a region"""
-        return [(r[0] * self.width_ratio, r[1] * self.height_ratio) for r in region[1]]
-
-    def _svg_to_polygon(self):
+    def _svg_to_polygons(self):
         """Converts the svg map file to a list of polygons"""
         polygons = []
         groups = parse(self.image)
-        #polygons = [[self._get_region_coords(region) for region in groups[g]] for g in groups]
+
         for g in groups:
             for path in groups[g]:
-                polygons.append(path)
+                points = [(p[0]*self.width_ratio, p[1]*self.height_ratio) for p in path[1]]
+                polygons.append(points)
+
         return polygons
 
     def get_map(self):
-        return self._svg_to_polygon()
+        return self._svg_to_polygons()
 
 class UsaMap(Map):
     def __init__(self):
         Map.__init__(self)
-        self.image = './gfx/maps/usa.svg'
+        self.image = './gfx/map_usa.svg'
 
 class ChinaMap(Map):
     def __init__(self):
-        pass
+        Map.__init__(self)
+        self.image = './gfx/map_china.svg'
 
 
---
+++ scripts/game.py
@@ -0,0 +1,8 @@
+from classes.gui import *
+
+def main():
+    gui = Gui()
+    gui.load_gfx()
+    gui.game_loop()
+
+if __name__ == '__main__': main()
